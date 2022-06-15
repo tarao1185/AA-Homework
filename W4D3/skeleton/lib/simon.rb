ae298d6da67afe4551925cfd97658c951f5c1457
tarao <tara_m_oliver@yahoo.com>
2022-06-14 23:33:43 -0400

    passed all specs

---
+++ W4D3/skeleton/lib/simon.rb
@@ -0,0 +1,57 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    until @game_over
+      take_turn
+    end
+
+    if @game_over
+      game_over_message
+      reset_game
+    end
+  end
+
+  def take_turn
+    if !@game_over
+      show_sequence
+      require_sequence
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    seq << COLORS.sample
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
