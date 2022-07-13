1292404740e12233f2c3cc42f55937fcf8e6f6bf
tarao1185 <105677363+tarao1185@users.noreply.github.com>
2022-07-13 01:39:41 -0400

    Add files via upload

---
+++ W8D3/Addtl_JS_problems.js
@@ -0,0 +1,7 @@
+//Write a function titleize that takes an array of names and a function (callback). titleize should use Array.prototype.map to create a new array full of titleized versions of each name - titleize meaning "Roger" should be made to read "Mx. Roger Jingleheimer Schmidt". Then pass this new array of names to the callback, which should use Array.prototype.forEach to print out each titleized name.
+
+function inject(arr, func) {
+   newArr =  arr.map((ele)=> `Mx. ${ele} Jingleheimer Schmidt`);
+   newArr.forEach(func(ele));
+   console.log(newArr)
+}
